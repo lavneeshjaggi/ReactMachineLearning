1142d4f24fa684df70f4a3a91dd8ea933ddf6305
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2021-06-18 19:42:03 +0530

    load, format and visualise the input data. define the model architecture. prepare the data for training. improve comments

--- src/tensorflow/tensorflow.js
+++ src/tensorflow/tensorflow.js
@@ -5,13 +5,7 @@ import * as tfvis from '@tensorflow/tfjs-vis';
 import './tensorflow.scss';
 
 class Tensorflow extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {} 
-    }  
-
-    // Load, Format and Visualise the input data
+    // 1. LOAD, FORMAT AND VISUALISE THE INPUT DATA
     async run() {
         // Get the car data reduced to just the variables we are interested and cleaned of missing data.
         async function getData() {
@@ -42,31 +36,80 @@ class Tensorflow extends React.Component {
             height: 300
           }
         );
+    } 
 
-        // Define the model architecture
+    // 2. DEFINE THE MODEL ARCHITECTURE
+    create() {
         function createModel() {
             // Create a sequential model
             const model = tf.sequential();
-        
+
             // Add a single input layer
             model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
-        
+
             // Add an output layer
             model.add(tf.layers.dense({units: 1, useBias: true}));
-        
+
             return model;
         } 
-      
+
         // Create the model
         const model = createModel();
         tfvis.show.modelSummary({name: 'Model Summary'}, model);
-    }     
+    }  
+    
+    // 3. PREPARE THE DATA FOR TRAINING
+    /**
+     * Convert the input data to tensors that we can use for machine
+     * learning. We will also do the important best practices of _shuffling_
+     * the data and _normalizing_ the data
+     * MPG on the y-axis.
+    */
+    convert() {
+        function convertToTensor(data) {
+            // Wrapping these calculations in a tidy will dispose any
+            // intermediate tensors.
+        
+            return tf.tidy(() => {
+            // Step 1. Shuffle the data
+            tf.util.shuffle(data);
+        
+            // Step 2. Convert data to Tensor
+            const inputs = data.map(d => d.horsepower)
+            const labels = data.map(d => d.mpg);
+        
+            const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+            const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+        
+            //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+            const inputMax = inputTensor.max();
+            const inputMin = inputTensor.min();
+            const labelMax = labelTensor.max();
+            const labelMin = labelTensor.min();
+        
+            const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+            const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+        
+            return {
+                inputs: normalizedInputs,
+                labels: normalizedLabels,
+                // Return the min/max bounds so we can use them later.
+                inputMax,
+                inputMin,
+                labelMax,
+                labelMin,
+            }
+            });
+        }
+    }
 
     render() {
         return (
             <div className='tensorflow'>
                 <h1>Hello Tensorflow</h1>
                 <button className="button" onClick={this.run}>Visualise Data</button>
+                <button className="button" onClick={this.create}>Create Model</button>
+                <button className="button" onClick={this.convert}>Convert Model</button>
             </div>
         )
     }
