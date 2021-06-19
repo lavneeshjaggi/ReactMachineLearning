248612d4b207d06f59435ac39b309e5d07738cbb
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2021-06-19 14:19:41 +0530

    make a function which will be used by the user to make predictions

--- src/tensorflow/tensorflow.js
+++ src/tensorflow/tensorflow.js
@@ -75,7 +75,10 @@ class Tensorflow extends React.Component {
             const model = tf.sequential();
 
             // Add a single input layer
-            model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+            model.add(tf.layers.dense({inputShape: [1], units: 64, useBias: true}));
+
+            model.add(tf.layers.dense({units: 32, activation: 'sigmoid'}));
+            model.add(tf.layers.dense({units: 32, activation: 'sigmoid'}));
 
             // Add an output layer
             model.add(tf.layers.dense({units: 1, useBias: true}));
@@ -95,34 +98,34 @@ class Tensorflow extends React.Component {
             // intermediate tensors.
         
             return tf.tidy(() => {
-            // Step 1. Shuffle the data
-            tf.util.shuffle(data);
-        
-            // Step 2. Convert data to Tensor
-            const inputs = data.map(d => d.horsepower)
-            const labels = data.map(d => d.mpg);
-        
-            const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
-            const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
-        
-            //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
-            const inputMax = inputTensor.max();
-            const inputMin = inputTensor.min();
-            const labelMax = labelTensor.max();
-            const labelMin = labelTensor.min();
-        
-            const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
-            const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
-        
-            return {
-                inputs: normalizedInputs,
-                labels: normalizedLabels,
-                // Return the min/max bounds so we can use them later.
-                inputMax,
-                inputMin,
-                labelMax,
-                labelMin,
-            }
+              // Step 1. Shuffle the data
+              tf.util.shuffle(data);
+          
+              // Step 2. Convert data to Tensor
+              const inputs = data.map(d => d.horsepower)
+              const labels = data.map(d => d.mpg);
+          
+              const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+              const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+          
+              //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+              const inputMax = inputTensor.max();
+              const inputMin = inputTensor.min();
+              const labelMax = labelTensor.max();
+              const labelMin = labelTensor.min();
+          
+              const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+              const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+          
+              return {
+                  inputs: normalizedInputs,
+                  labels: normalizedLabels,
+                  // Return the min/max bounds so we can use them later.
+                  inputMax,
+                  inputMin,
+                  labelMax,
+                  labelMin,
+              }
             });
         }
 
@@ -135,8 +138,8 @@ class Tensorflow extends React.Component {
               metrics: ['mse'],
             });
           
-            const batchSize = 32;
-            const epochs = 50;
+            const batchSize = 64;
+            const epochs = 100;
           
             return await model.fit(inputs, labels, {
               batchSize,
@@ -158,10 +161,9 @@ class Tensorflow extends React.Component {
             // We un-normalize the data by doing the inverse of the min-max scaling
             // that we did earlier.
             const [xs, preds] = tf.tidy(() => {
-          
               const xs = tf.linspace(0, 1, 100);
               const preds = model.predict(xs.reshape([100, 1]));
-          
+
               const unNormXs = xs
                 .mul(inputMax.sub(inputMin))
                 .add(inputMin);
@@ -174,11 +176,10 @@ class Tensorflow extends React.Component {
               return [unNormXs.dataSync(), unNormPreds.dataSync()];
             });
           
-          
             const predictedPoints = Array.from(xs).map((val, i) => {
               return {x: val, y: preds[i]}
             });
-          
+
             const originalPoints = inputData.map(d => ({
               x: d.horsepower, y: d.mpg,
             }));
@@ -195,6 +196,30 @@ class Tensorflow extends React.Component {
             );
         }
 
+        function makePrediction() {
+          var { inputMin, inputMax, labelMin, labelMax } = tensorData;
+
+          const val = 60;
+
+          const num = (val - inputMin.dataSync()) / (inputMax.dataSync() - inputMin.dataSync());
+
+          console.log("num: " + num);
+          console.log("inputMin: " + inputMin);
+          console.log("inputMax: " + inputMax);
+
+          const preds = model.predict(tf.tensor2d([num], [1, 1]));
+
+          console.log("preds: " + preds);
+
+          console.log("preds Data Sync: " + preds.dataSync());
+
+          const unNormPreds = preds.mul(labelMax.sub(labelMin)).add(labelMin);
+
+          console.log("unNormPreds: " + unNormPreds);
+
+          console.log(unNormPreds.dataSync());
+        }
+
         return (
             <div className='tensorflow'>
                 <h1>Hello Tensorflow</h1>
@@ -202,6 +227,7 @@ class Tensorflow extends React.Component {
                 <button className="button" onClick={createAndConvertModel}>Create and Convert Model</button>
                 <button className="button" onClick={trainTheModel}>Train Model</button>
                 <button className="button" onClick={testTheModel}>Test Model</button>
+                <button className="button" onClick={makePrediction}>Make Prediction</button>
             </div>
         )
     }
