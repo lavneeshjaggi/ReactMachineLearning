d57c46bd202ad6d79b1d4a4678499793f1643f84
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2021-06-19 23:10:43 +0530

    create setup for handwriting prediction

--- src/pages/tensorflow handwriting/tensorflow.handwriting.jsx
+++ src/pages/tensorflow handwriting/tensorflow.handwriting.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './tensorflow.handwriting.styles.scss';
 
-const TensorflowHandwriting = () => (
-    <div classname="tensorflowhandwriting">
-        <h1>Tensorflow Handwriting</h1>
-    </div>
-);
+class TensorflowHandwriting extends React.Component {
+    render() {
+        return (
+            <div classname="tensorflowhandwriting">
+                <h1>Tensorflow Handwriting</h1>
+            </div>
+        )
+    }
+}
 
 export default TensorflowHandwriting;
