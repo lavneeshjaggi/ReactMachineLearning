0d74a1f3b2fdc0530a41f00e211f891dee97ae89
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2021-06-20 20:39:56 +0530

    add provision for users to enter custom images and style the page

--- src/pages/tensorflow handwriting/tensorflow.handwriting.jsx
+++ src/pages/tensorflow handwriting/tensorflow.handwriting.jsx
@@ -10,14 +10,27 @@ class TensorflowHandwriting extends React.Component {
     super();
 
     this.state = {
-      loading: true
+      loading: true,
+      image: undefined,
+      answer: null
     }
   }
 
   async componentDidMount() {
-    await prepareTheModel();
+    // await prepareTheModel();
 
     this.setState({ loading: false });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  handleImageChange = (event) => {
+
+    if(event.target.files && event.target.files[0]) {
+      this.setState({ image: URL.createObjectURL(event.target.files[0]) })
+    }
   }
 
   render() {
@@ -27,7 +40,18 @@ class TensorflowHandwriting extends React.Component {
 
     return (
         <div className="tensorflowhandwriting">
-            <h1>Tensorflow Handwriting</h1>
+          <h1>Tensorflow Handwriting</h1>
+          <h1 className="prediction">Prediction: {this.state.answer}</h1>
+          <img className="image" src={this.state.image} alt="Image Will Be Displayed Here" />
+          <form className="form" onSubmit={this.handleSubmit}>
+            <input 
+              className="input" 
+              name="file" 
+              onChange={this.handleImageChange} 
+              type="file" 
+            />
+            <button className="button">Submit</button>
+          </form>
         </div>
     )
   }
