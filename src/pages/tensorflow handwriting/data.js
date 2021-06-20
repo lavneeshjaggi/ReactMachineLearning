fb2c226e8bb749c68f0b6ba42796f399187cffaf
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2021-06-21 01:02:58 +0530

    update syntax

--- src/pages/tensorflow handwriting/data.js
+++ src/pages/tensorflow handwriting/data.js
@@ -61,68 +61,68 @@ export class MnistData {
                 this.datasetImages = new Float32Array(datasetBytesBuffer);
 
                 resolve();
-        };
-        img.src = MNIST_IMAGES_SPRITE_PATH;
-    });
-
-    const labelsRequest = fetch(MNIST_LABELS_PATH);
-    const [imgResponse, labelsResponse] =
-        await Promise.all([imgRequest, labelsRequest]);
-
-    this.datasetLabels = new Uint8Array(await labelsResponse.arrayBuffer());
-    this.datasetimg = new Uint8Array(await imgResponse);
-
-    // Create shuffled indices into the train/test set for when we select a
-    // random dataset element for training / validation.
-    this.trainIndices = tf.util.createShuffledIndices(NUM_TRAIN_ELEMENTS);
-    this.testIndices = tf.util.createShuffledIndices(NUM_TEST_ELEMENTS);
-
-    // Slice the the images and labels into train and test sets.
-    this.trainImages =
-        this.datasetImages.slice(0, IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
-    this.testImages = this.datasetImages.slice(IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
-    this.trainLabels =
-        this.datasetLabels.slice(0, NUM_CLASSES * NUM_TRAIN_ELEMENTS);
-    this.testLabels =
-        this.datasetLabels.slice(NUM_CLASSES * NUM_TRAIN_ELEMENTS);
-}
-
-nextTrainBatch(batchSize) {
-    return this.nextBatch(
-        batchSize, [this.trainImages, this.trainLabels], () => {
-            this.shuffledTrainIndex =
-                (this.shuffledTrainIndex + 1) % this.trainIndices.length;
-            return this.trainIndices[this.shuffledTrainIndex];
+            };
+            img.src = MNIST_IMAGES_SPRITE_PATH;
         });
-}
 
-nextTestBatch(batchSize) {
-    return this.nextBatch(batchSize, [this.testImages, this.testLabels], () => {
-        this.shuffledTestIndex =
-            (this.shuffledTestIndex + 1) % this.testIndices.length;
-        return this.testIndices[this.shuffledTestIndex];
-    });
-}
+        const labelsRequest = fetch(MNIST_LABELS_PATH);
+        const [imgResponse, labelsResponse] =
+            await Promise.all([imgRequest, labelsRequest]);
 
-nextBatch(batchSize, data, index) {
-    const batchImagesArray = new Float32Array(batchSize * IMAGE_SIZE);
-    const batchLabelsArray = new Uint8Array(batchSize * NUM_CLASSES);
+        this.datasetLabels = new Uint8Array(await labelsResponse.arrayBuffer());
+        this.datasetimg = new Uint8Array(await imgResponse);
 
-    for (let i = 0; i < batchSize; i++) {
-        const idx = index();
+        // Create shuffled indices into the train/test set for when we select a
+        // random dataset element for training / validation.
+        this.trainIndices = tf.util.createShuffledIndices(NUM_TRAIN_ELEMENTS);
+        this.testIndices = tf.util.createShuffledIndices(NUM_TEST_ELEMENTS);
 
-        const image =
-            data[0].slice(idx * IMAGE_SIZE, idx * IMAGE_SIZE + IMAGE_SIZE);
-        batchImagesArray.set(image, i * IMAGE_SIZE);
+        // Slice the the images and labels into train and test sets.
+        this.trainImages =
+            this.datasetImages.slice(0, IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+        this.testImages = this.datasetImages.slice(IMAGE_SIZE * NUM_TRAIN_ELEMENTS);
+        this.trainLabels =
+            this.datasetLabels.slice(0, NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+        this.testLabels =
+            this.datasetLabels.slice(NUM_CLASSES * NUM_TRAIN_ELEMENTS);
+    }
 
-        const label =
-            data[1].slice(idx * NUM_CLASSES, idx * NUM_CLASSES + NUM_CLASSES);
-        batchLabelsArray.set(label, i * NUM_CLASSES);
+    nextTrainBatch(batchSize) {
+        return this.nextBatch(
+            batchSize, [this.trainImages, this.trainLabels], () => {
+                this.shuffledTrainIndex =
+                    (this.shuffledTrainIndex + 1) % this.trainIndices.length;
+                return this.trainIndices[this.shuffledTrainIndex];
+            });
+    }
+
+    nextTestBatch(batchSize) {
+        return this.nextBatch(batchSize, [this.testImages, this.testLabels], () => {
+            this.shuffledTestIndex =
+                (this.shuffledTestIndex + 1) % this.testIndices.length;
+            return this.testIndices[this.shuffledTestIndex];
+        });
+    }
+
+    nextBatch(batchSize, data, index) {
+        const batchImagesArray = new Float32Array(batchSize * IMAGE_SIZE);
+        const batchLabelsArray = new Uint8Array(batchSize * NUM_CLASSES);
+
+        for (let i = 0; i < batchSize; i++) {
+            const idx = index();
+
+            const image =
+                data[0].slice(idx * IMAGE_SIZE, idx * IMAGE_SIZE + IMAGE_SIZE);
+                batchImagesArray.set(image, i * IMAGE_SIZE);
+
+            const label =
+                data[1].slice(idx * NUM_CLASSES, idx * NUM_CLASSES + NUM_CLASSES);
+                batchLabelsArray.set(label, i * NUM_CLASSES);
         }
 
-        const xs = tf.tensor2d(batchImagesArray, [batchSize, IMAGE_SIZE]);
-        const labels = tf.tensor2d(batchLabelsArray, [batchSize, NUM_CLASSES]);
+            const xs = tf.tensor2d(batchImagesArray, [batchSize, IMAGE_SIZE]);
+            const labels = tf.tensor2d(batchLabelsArray, [batchSize, NUM_CLASSES]);
 
-        return {xs, labels};
+            return {xs, labels};
     }
 }
